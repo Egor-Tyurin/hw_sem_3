016e47c385c109005dab9339051f6f92a6376296
Egor-Tyurin <127549578+Egor-Tyurin@users.noreply.github.com>
2024-03-21 03:20:48 +0400

    Add files via upload
    Лабораторная 4. Матрицы и типы данных. 4 Семестр

---
+++ Second_maximum_index.h
@@ -0,0 +1,9 @@
+#ifndef SECOND_MAX_INDEX_H
+#define SECOND_MAX_INDEX_H
+
+template <typename T>
+int findSecondMaxIndex(T* array, int size);
+
+#include "Second_maximum_index.tpp"
+
+#endif // SECOND_MAX_INDEX_H
